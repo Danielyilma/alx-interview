4315ee43ab7baafcd9aff468ab95636c8df1f7b2
Danielyilma <yilmad752@gmail.com>
2024-07-04 11:35:46 +0300

    Lockboxes

--- 0x00-pascal_triangle/0x01-lockboxes/0-lockboxes.py
+++ 0x00-pascal_triangle/0x01-lockboxes/0-lockboxes.py
@@ -2,44 +2,28 @@
 '''implementation for the lockbox question'''
 
 
+
 def canUnlockAll(boxes):
-    '''locked boxes'''
-    if not boxes:
-        return False
-
+    '''
+    It addes the first box keys to a set(keys) and for each keys, it checkes
+    there is a box that can open it and  then it addes the keys in the opened
+    box to the set(keys) and repeates the process until all boxes are opened
+    or there is no key left
+    '''
     keys = set(boxes[0])
-    checked_boxes = set([0])
-    total_boxes = len(boxes)
+    checked_box = [0]
+    box_len = len(boxes) - 1
 
-    while keys:
-        key = keys.pop()
-        if key < total_boxes and key not in checked_boxes:
-            checked_boxes.add(key)
-            keys.update(boxes[key])
+    while len(keys) and box_len:
+        key = next(iter(keys))
+        if key in checked_box:
+            keys.remove(key)
+            continue
 
-    return len(checked_boxes) == total_boxes
+        if key < len(boxes):
+            keys.update((boxes[key]))
+            checked_box.append(key)
+            box_len -= 1
+        keys.remove(key)
 
-# def canUnlockAll(boxes):
-#     '''
-#     It addes the first box keys to a set(keys) and for each keys, it checkes
-#     there is a box that can open it and  then it addes the keys in the opened
-#     box to the set(keys) and repeates the process until all boxes are opened
-#     or there is no key left
-#     '''
-#     keys = set(boxes[0])
-#     checked_box = [0]
-#     box_len = len(boxes) - 1
-
-#     while len(keys) and box_len:
-#         key = next(iter(keys))
-#         if key in checked_box:
-#             keys.remove(key)
-#             continue
-
-#         if key < len(boxes):
-#             keys.update((boxes[key]))
-#             checked_box.append(key)
-#             box_len -= 1
-#         keys.remove(key)
-
-#     return box_len == 0
+    return box_len == 0

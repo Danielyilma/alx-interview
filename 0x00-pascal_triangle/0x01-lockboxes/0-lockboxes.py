b27be3f53d547671a7f2d0cd37ae5b1707eea362
Danielyilma <yilmad752@gmail.com>
2024-07-04 11:33:03 +0300

    Lockboxes

--- 0x00-pascal_triangle/0x01-lockboxes/0-lockboxes.py
+++ 0x00-pascal_triangle/0x01-lockboxes/0-lockboxes.py
@@ -3,26 +3,43 @@
 
 
 def canUnlockAll(boxes):
-    '''
-    It addes the first box keys to a set(keys) and for each keys, it checkes
-    there is a box that can open it and  then it addes the keys in the opened
-    box to the set(keys) and repeates the process until all boxes are opened
-    or there is no key left
-    '''
+    '''locked boxes'''
+    if not boxes:
+        return False
+
     keys = set(boxes[0])
-    checked_box = [0]
-    box_len = len(boxes) - 1
+    checked_boxes = set([0])
+    total_boxes = len(boxes)
 
-    while len(keys) and box_len:
-        key = next(iter(keys))
-        if key in checked_box:
-            keys.remove(key)
-            continue
+    while keys:
+        key = keys.pop()
+        if key < total_boxes and key not in checked_boxes:
+            checked_boxes.add(key)
+            keys.update(boxes[key])
 
-        if key < len(boxes):
-            keys.update((boxes[key]))
-            checked_box.append(key)
-            box_len -= 1
-        keys.remove(key)
+    return len(checked_boxes) == total_boxes
 
-    return box_len == 0
+# def canUnlockAll(boxes):
+#     '''
+#     It addes the first box keys to a set(keys) and for each keys, it checkes
+#     there is a box that can open it and  then it addes the keys in the opened
+#     box to the set(keys) and repeates the process until all boxes are opened
+#     or there is no key left
+#     '''
+#     keys = set(boxes[0])
+#     checked_box = [0]
+#     box_len = len(boxes) - 1
+
+#     while len(keys) and box_len:
+#         key = next(iter(keys))
+#         if key in checked_box:
+#             keys.remove(key)
+#             continue
+
+#         if key < len(boxes):
+#             keys.update((boxes[key]))
+#             checked_box.append(key)
+#             box_len -= 1
+#         keys.remove(key)
+
+#     return box_len == 0

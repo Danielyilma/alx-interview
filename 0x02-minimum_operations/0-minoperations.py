f6e89b91d179fce0aec417dd2d0abb81f1e54097
Danielyilma <yilmad752@gmail.com>
2024-07-10 21:19:38 +0300

    minimum operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
+'''implement minimum operation problem'''
 
 def minOperations(n):
+    '''returns minimum operation needed to copy a character n times'''
     if n == 0 or n == 1:
         return 0
-    
+
     oper = 2
     h_count = 2
     inc = 1

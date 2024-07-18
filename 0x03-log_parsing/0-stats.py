43f9a4f3d5ea2f65a47d0c94ff675d8cd1db31b5
Danielyilma <yilmad752@gmail.com>
2024-07-18 20:20:32 +0300

    log parsimg

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
 '''parsing log data content'''
 import re
-import signal
+import signal, sys
 from typing import List, Tuple
 
 
-result = [] 
-
-
-def print_list(logs: List[Tuple[int, int]]) -> None:
+def print_list(logs: List[Tuple[int, int]], filesizes: int) -> None:
     '''
         print the value of a list in this format
         <status code>: <number>
     '''
-    sum = 0
-    for log in logs:
-        sum += int(log[1])
-    print(f'File size: {sum}')
-    for log in logs:
-        print(f"{log[0]}: {log[1]}")
+    totalsize = sum(filesizes)
+    sorted(logs)
+    print(f'File size: {totalsize}')
+    for key in sorted(logs):
+        print(f"{key}: {logs[key]}")
 
 
 def main():
@@ -26,23 +22,25 @@ def main():
 
     format_pattern = '(\d{1,3}\.?){3}\d{1,3} - \[\d{4,}(-\d{1,2}){2} \
 (\d{1,2}:){2}\d{1,2}.\d{1,8}\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d{1,4})'
+    result = {}
+    sizes = []
     try:
-        while True:
-            result = []
-            for i in range(10):
-                line = input()
-                match = re.match(format_pattern, line)
-                if not match:
-                    continue
+        count = 0
+        for line in sys.stdin:
+            match = re.match(format_pattern, line)
+            if not match:
+                continue
+            result[match.group(4)] = result.get(match.group(4), 0) + 1
+            sizes.append(int(match.group(5)))
+            count += 1
 
-                result.append((match.group(4), match.group(5)))
+            if count % 10 == 0:
+                print_list(result, sizes)
 
-            result.sort(key=lambda x: x[0])
-            print_list(result)
+        print_list(result, sizes)
     except KeyboardInterrupt:
-        result.sort(key=lambda x: x[0])
-        print_list(result)
-        main()
+        print_list(result, sizes)
+        sys.exit(0)
 
 
 if __name__ == "__main__":

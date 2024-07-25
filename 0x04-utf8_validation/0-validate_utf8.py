585eee5d9a17ff493931e67c4e84c8358a4db6be
Danielyilma <yilmad752@gmail.com>
2024-07-25 14:21:06 +0300

    validating utf-8 encoding

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,8 +4,11 @@
 
 def validUTF8(data):
     '''validating utf-8 encoding'''
+    if data is None:
+        return False
+
     for num in data:
-        if (num >> 7) >= 1:
+        if (num >> 7) != 0:
             return False
 
     return True

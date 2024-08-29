db180426dcbdc08975aeb2625ac852d92258814e
Danielyilma <yilmad752@gmail.com>
2024-08-29 12:41:36 +0300

    finding the perimeter of an island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,20 +6,20 @@ def island_perimeter(grid):
     '''finds the island perimeter'''
     perimeter = 0
 
-    for row in range(1, len(grid) - 1):
-        for col in range(1, len(grid[row]) - 1):
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
             if grid[row][col] != 1:
                 continue
-            if grid[row - 1][col] == 0:
+            if row - 1 < 0 or grid[row - 1][col] == 0:
                 perimeter += 1
 
-            if grid[row + 1][col] == 0:
+            if row + 1 >= len(grid) or grid[row + 1][col] == 0:
                 perimeter += 1
 
-            if grid[row][col - 1] == 0:
+            if col - 1 < 0 or grid[row][col - 1] == 0:
                 perimeter += 1
 
-            if grid[row][col + 1] == 0:
+            if col + 1 >= len(grid[row]) or grid[row][col + 1] == 0:
                 perimeter += 1
 
     return perimeter
